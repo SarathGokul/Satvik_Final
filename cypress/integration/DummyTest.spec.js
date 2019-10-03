83f6a742181b64747ddf1808213d66a61aac41ae
satwik <tasks@techjini.com>
2019-10-03 17:59:05 +0530

    whole project committed

---
+++ cypress/integration/DummyTest.spec.js
@@ -0,0 +1,11 @@
+
+
+context('Dummy test',() =>{
+it('satwik 1st testcase',()=>{
+    cy.visit('https://www.cypress.io/');
+})
+
+it('satwik 2nd testcase',()=>{
+    cy.visit('https://www.phptravels.net');
+})
+})
